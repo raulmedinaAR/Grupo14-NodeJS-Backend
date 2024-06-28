30370328a13ba602c7325e59914aa907b144c43d
ibeza <jose.claros@hotmail.com>
2024-06-28 18:26:10 -0300

    actualizacion

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -20,6 +20,7 @@ const getProducts =  (genderId, response) => {
     dbConnection.query(query, (error, results, fields) => {
         if (error) {
           console.error('ejecutando al consulta:', error);
+          response.status(501).send('Error Interno de la BD');
           return;
         }
 
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,10 @@
 const express = require ('express'); 
-const {getProductsMen} = require('./controllers/productsController');
+const {getProductsMen, getProductsWoman} = require('../controllers/productsController');
 const router = express.Router();
 
 
 router.get('/men', getProductsMen);
 router.get('/woman', getProductsWoman);
+
+module.exports = router;
 
