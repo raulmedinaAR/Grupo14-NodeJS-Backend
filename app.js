1f3ea8e0f812ff0c9ca6f0067bfca30ad441d25b
Raúl Medina <raul@medina.net.ar>
2024-07-01 18:40:03 -0300

    update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use('/', (request, response) => {
     response.status(200).send(`<br><br><br><center><h1><b>Servidor activo!!!</b></h1><h3>${request.method}:&nbsp;&nbsp;&nbsp;${request.url}</h3></center>`);
 });
 
-const PORT = process.env.SERVER_PORT || 3030;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
     console.log('servidor activo en el PORT: ' + PORT);
 });
