aab56cdabb67114b659eb0d2612257978d555665
Raúl Medina <raul@medina.net.ar>
2024-07-02 11:48:44 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const packageJson = require('../package.json');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // Middleware para parsear las solicitudes entrantes JSON
 
 const productsRoutes      = require('./routes/productsRoutes');
